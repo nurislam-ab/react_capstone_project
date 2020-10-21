0f6942abbc50d063476338001385acaae0b8ecef
Nurislam Ab <abdurasulovnurislam@gmail.com>
2020-10-21 11:59:35 +0500

    Add base header and navbar components

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  console.log('reach header');
+
+  return (
+    <header>
+      <h2>This is header</h2>
+    </header>
+  );
+};
+
+export default Header;
