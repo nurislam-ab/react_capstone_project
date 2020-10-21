7af6158e0b32f6baf46c97f475063077f972953f
Nurislam Ab <abdurasulovnurislam@gmail.com>
2020-10-21 12:05:51 +0500

    Add initital state method and some data to test the app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './assets/scss/index.css';
+import './assets/scss/index.scss';
 import App from './components/App';
 
+const plantIds = () => Math.ceil(Math.random() * 100);
+
+const plantTitles = [
+  'Almond',
+  'Bean',
+  'Baobab',
+  'Wineberry',
+  'Tomato',
+  'Strawberry',
+  'Mango',
+];
+
+const families = [
+  'Beech',
+  'Nettle',
+  'Grass',
+  'Aster',
+  'Buttercup',
+  'Pea',
+  'Olive',
+];
+
+const contentRandom = () => Math.floor(Math.random() * families.length);
+
+const plants = [];
+
+for (let i = 0; i < plantTitles.length; i += 1) {
+  plants[i] = {
+    plantId: plantIds(),
+    title: plantTitles[i],
+    content: families[contentRandom()],
+  };
+}
+
+const initialState = {
+  plants,
+};
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root'),
 );
