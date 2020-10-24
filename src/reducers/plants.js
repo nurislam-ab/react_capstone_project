b030714cc5f25ba5fa007719b581021d71ed490f
Nurislam Ab <abdurasulovnurislam@gmail.com>
2020-10-24 10:17:31 +0500

    Add plants reducer

--- src/reducers/plants.js
+++ src/reducers/plants.js
@@ -1,5 +1,36 @@
-const plantReducer = (state = [], action) => {
+import {
+  FETCH_INIT,
+  FETCH_PLANTS,
+  FETCH_FAIL,
+} from '../actions/actionTypes';
+
+const initialState = {
+  loading: false,
+  error: false,
+  plants: [],
+};
+
+const plantReducer = (state = initialState, action) => {
   switch (action.type) {
+    case FETCH_INIT:
+      return {
+        ...state,
+        loading: true,
+        error: false,
+      };
+    case FETCH_PLANTS:
+      return {
+        ...state,
+        loading: false,
+        error: false,
+        plants: action.payload.plants,
+      };
+    case FETCH_FAIL:
+      return {
+        ...state,
+        loading: false,
+        error: true,
+      };
     default:
       return state;
   }
