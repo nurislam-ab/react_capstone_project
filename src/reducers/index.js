c276fb7091de84fcafcb400068a75a035d15be32
Nurislam Ab <abdurasulovnurislam@gmail.com>
2020-10-24 10:17:48 +0500

    Combine all the reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
+import categoryReducer from './categories';
 import plantReducer from './plants';
+import filterReducer from './filter';
 
 const rootReducer = combineReducers({
   plants: plantReducer,
+  categories: categoryReducer,
+  filterReducer,
 });
 
 export default rootReducer;
