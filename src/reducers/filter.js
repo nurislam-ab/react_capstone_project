5724bcd7125169300c7ae42b4e825115eab534c6
Nurislam Ab <abdurasulovnurislam@gmail.com>
2020-10-24 10:17:17 +0500

    Implement filter reducer

---
+++ src/reducers/filter.js
@@ -0,0 +1,15 @@
+import { CHANGE_FILTER } from '../actions/actionTypes';
+
+const initialState = 'All';
+
+const filterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CHANGE_FILTER:
+      console.log('filter from the change filter', action.payload.filter);
+      return action.payload.filter;
+    default:
+      return state;
+  }
+};
+
+export default filterReducer;
