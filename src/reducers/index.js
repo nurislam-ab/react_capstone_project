56aa395e36cd2c7a5b625f2a5e8d565019212480
Nurislam Ab <abdurasulovnurislam@gmail.com>
2020-10-27 23:55:46 +0500

    Combine plant selected and plant item reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,15 @@ import { combineReducers } from 'redux';
 import categoryReducer from './categories';
 import plantReducer from './plants';
 import filterReducer from './filter';
+import plantItemReducer from './plant';
+import plantSelectedReducer from './plantSelected';
 
 const rootReducer = combineReducers({
   plants: plantReducer,
   categories: categoryReducer,
   filterReducer,
+  plantItemReducer,
+  plantSelectedReducer,
 });
 
 export default rootReducer;
