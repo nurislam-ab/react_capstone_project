7056e0726f5235e83a961588676461d42544feb6
Nurislam Ab <abdurasulovnurislam@gmail.com>
2020-10-27 23:58:03 +0500

    Remove console logs

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,9 @@
 import React from 'react';
 
-const Header = () => {
-  console.log('reach header');
-
-  return (
-    <header>
-      <h2>This is header</h2>
-    </header>
-  );
-};
+const Header = () => (
+  <header>
+    <h2>This is header</h2>
+  </header>
+);
 
 export default Header;
