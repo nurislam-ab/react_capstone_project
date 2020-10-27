ea29c96f759bc8e7c21c42db8020bb805b3ff109
Nurislam Ab <abdurasulovnurislam@gmail.com>
2020-10-28 00:05:13 +0500

    Remove test data and import Browser router

--- src/index.js
+++ src/index.js
@@ -1,54 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 import './assets/scss/index.scss';
 import App from './components/App';
 import rootReducer from './reducers/index';
 
-const plantIds = () => Math.ceil(Math.random() * 100);
-
-const plantTitles = [
-  'Almond',
-  'Bean',
-  'Baobab',
-  'Wineberry',
-  'Tomato',
-  'Strawberry',
-  'Mango',
-];
-
-const families = [
-  'Beech',
-  'Nettle',
-  'Grass',
-  'Aster',
-  'Buttercup',
-  'Pea',
-  'Olive',
-];
-
-const contentRandom = () => Math.floor(Math.random() * families.length);
-
-const plants = [];
-
-for (let i = 0; i < plantTitles.length; i += 1) {
-  plants[i] = {
-    plantId: plantIds(),
-    title: plantTitles[i],
-    content: families[contentRandom()],
-  };
-}
-
-const initialState = {
-  plants,
-};
-
-const store = createStore(rootReducer, initialState);
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger, thunk)),
+);
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root'),
 );
