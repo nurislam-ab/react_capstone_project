d47488d9e74d199b7fe2c15aef2c4bf1d49f933f
Nurislam Ab <abdurasulovnurislam@gmail.com>
2020-10-28 00:03:15 +0500

    Develop cateory filter component

---
+++ src/components/CategoryFilter.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const CategoryFilter = ({ filter, onFilter, categories }) => {
+  const handleFilter = ({ target }) => {
+    onFilter(target.value);
+  };
+
+  const renderCategoriesList = categories.map(category => {
+    if (category.common_name !== null) {
+      return (
+        <option
+          key={category.id}
+          id={category.common_name}
+          name={category.common_name}
+        >
+          {category.common_name}
+        </option>
+      );
+    }
+
+    return (
+      <option
+        key={category.id}
+        id={category.name}
+        name={category.name}
+      >
+        {category.name}
+      </option>
+    );
+  });
+
+  return (
+    <section>
+      <h2>Categories</h2>
+      <select className="categories-list" onChange={handleFilter}>
+        <option name="All" key="All">All</option>
+        {renderCategoriesList}
+      </select>
+    </section>
+  );
+};
+
+export default CategoryFilter;
