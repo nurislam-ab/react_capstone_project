3e0d972011be08cf095a0978ebad1a7fe5098fc0
Nurislam Ab <abdurasulovnurislam@gmail.com>
2020-10-28 21:40:48 +0500

    Change styles for the App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
-import './styles.scss';
+import './App.scss';
 import Home from '../Home/Home';
 import Plant from '../Plant/Plant';
 
