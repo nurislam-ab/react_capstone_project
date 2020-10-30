eaf0d120c8044fd29ad15574dad15cb74d22f774
Nurislam Ab <abdurasulovnurislam@gmail.com>
2020-10-31 01:01:06 +0500

    Define prop types for the meal and render data

--- src/components/Plant/Plant.js
+++ src/components/Plant/Plant.js
@@ -2,54 +2,35 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Plant.scss';
 
-const Plant = ({ plant }) => {
-  const plantName = plant.common_name === null || plant.common_name === '' ? plant.scientific_name : plant.common_name;
-
-  const vegetable = plant.vegetable === false ? 'not vegetable' : 'vegetable';
-  return (
-    <div className="plant-content">
-      <div className="plant-cover">
-        <div className="image-wrapper col-2">
-          <img src={plant.image_url} alt={plantName} className="image" />
-        </div>
-
-        <div className="main-info col-2">
-          <span className="family">{plant.family.name}</span>
-          <h1 className="title">{plantName}</h1>
-          <p>{plant.observations}</p>
-          <div className="vegetable-label-wrapper">
-            <span className="vegetable">{vegetable}</span>
-          </div>
-        </div>
+const Plant = ({ plant }) => (
+  <div className="plant-content">
+    <div className="plant-cover">
+      <div className="image-wrapper col-2">
+        <img src={plant.strMealThumb} alt={plant.strMeal} className="image" />
       </div>
 
-      <div className="short-info">
-        <div className="col-4 info-block">
-          <strong>{plant.scientific_name}</strong>
-          <span>scientific name</span>
-        </div>
-
-        <div className="col-4 info-block">
-          <strong>{plant.author}</strong>
-          <span>author</span>
-        </div>
-
-        <div className="col-4 info-block">
-          <strong>{plant.family_common_name}</strong>
-          <span>family common name</span>
-        </div>
-
-        <div className="col-4 info-block">
-          <strong>{plant.year}</strong>
-          <span>year found</span>
+      <div className="main-info col-2">
+        <span className="family">{plant.strCategory}</span>
+        <h1 className="title">{plant.strMeal}</h1>
+        <p>{plant.strInstructions}</p>
+        <div className="vegetable-label-wrapper">
+          <span className="vegetable">{plant.strTags}</span>
         </div>
       </div>
     </div>
-  );
-};
+  </div>
+);
 
-Plant.propTypes = {
-  plant: PropTypes.instanceOf(Object).isRequired,
-};
+Plant.propTypes = ({
+  plant: PropTypes.shape({
+    strMeal: PropTypes.string.isRequired,
+    strCategory: PropTypes.string,
+    strMealThumb: PropTypes.string.isRequired,
+    idMeal: PropTypes.string.isRequired,
+    strArea: PropTypes.string,
+    strInstructions: PropTypes.string,
+    strTags: PropTypes.string,
+  }).isRequired,
+});
 
 export default Plant;
